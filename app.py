0e706a5bc9f55872689ee6d2ed4dc8131a249002
ppalmc <56991473+ppalmc@users.noreply.github.com>
2019-12-13 17:32:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,9 @@ def showmenu():
     client = MongoClient("mongodb+srv://6131866021:1234@cluster0-3xijp.mongodb.net/test?retryWrites=true&w=majority")
     db = client.student_scores
     getstore = request.args.get('store')
-    docs = db.menus.find_one({'store':getstore})
-    ret = {'item':docs['item'], 'store':docs['store'], 'cal':docs['cal'], 'filter':docs['filter'], 'price':docs['price']}
-    return jsonify(ret)
+    docs = db.menus.find_all({'store':getstore})
+    r = dict()
+    for doc in docs:
+    		ret = {'item':doc['item'], 'store':doc['store'], 'cal':doc['cal'], 'filter':doc['filter'], 'price':doc['price']}
+    		r.append(ret)
+    return jsonify(r)
